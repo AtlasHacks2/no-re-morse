8ce4aee49b08a758e8b7ecc2895f552602836d2c
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2021-07-03 22:34:57 +0800

    Add constants file

--- Level1.py
+++ Level1.py
@@ -1,7 +1,13 @@
 from tkinter import *
 
+import HomePage
+
+
 class LevelOne:
     def __init__(self):
         self.root = Tk()
         self.root.geometry('500x500')
         self.root.title("Play")
+
+        self.label_1 = Label(self.home, text=HomePage.home_page.name, width=20, font=("bold", 10))
+        self.label_1.place(x=10, y=10)
--- Tutorial.py
+++ Tutorial.py
@@ -22,7 +22,7 @@ class tutorial:
         # CREATES LABEL FOR HYPERLINK
         link1 = Label(self.learn, text="LEARN MORSE CODE from a MEMORY CHAMP (in 15 minutes)", font=('Maiden Oranged', 10), fg="blue", cursor="hand2",bg="grey")
         link1.pack()
-        link1.bind("<Button-1>", lambda e:callback("https://www.youtube.com/watch?v=D8tPkb98Fkk"))
+        link1.bind("<Button-1>", lambda e: callback("https://www.youtube.com/watch?v=D8tPkb98Fkk"))
         # CREATES LABEL FOR HYPERLINK
         link1 = Label(self.learn, text="Morse Code Sound Effects(2 minute video)",font=('Maiden Orange', 10), fg="blue", cursor="hand2")
         link1.pack()
